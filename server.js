feb8cc81056e6b286f8a0d640feea0d70cd97a4c
Ariel <arcomp123@gmail.com>
2019-11-23 18:09:31 -0800

    Fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./db');
 const path = require('path');
-const PORT = Process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // const mysql = require('mysql2');
 
